4aa1208bacdf7c3a29be3990372a2af0433f81aa
kojeffi <omondijeff88@gmail.com>
2024-04-30 08:51:50 +0300

    updates

--- melianSafeSysAi/settings.py
+++ melianSafeSysAi/settings.py
@@ -152,8 +152,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
